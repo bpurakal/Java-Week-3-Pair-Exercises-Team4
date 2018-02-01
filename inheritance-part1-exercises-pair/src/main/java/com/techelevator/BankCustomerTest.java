aec595abf15a4752af00e0f9c0443a921b4419a1
Bryan Purakal <bpurakal@gmail.com>
2018-02-01 14:39:29 -0500

    junit paired

--- inheritance-part1-exercises-pair/src/main/java/com/techelevator/BankCustomerTest.java
+++ inheritance-part1-exercises-pair/src/main/java/com/techelevator/BankCustomerTest.java
@@ -2,6 +2,7 @@ package com.techelevator;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,12 +15,21 @@ public class BankCustomerTest {
 	private CheckingAccount check;
 	private SavingsAccount save;
 	private BankAccount bank;
-	
+	private BankAccount accounts;
 	
 	@Before
 	public void setUp() throws Exception {
 		customer = new BankCustomer("Joe", "123 Sesame St", "111-111-1111");
 		bank = new BankAccount();
+		check = new CheckingAccount();
+		save = new SavingsAccount();
+		check.deposit(new BigDecimal ("10000"));
+		save.deposit(new BigDecimal ("10000"));
+		bank.deposit(new BigDecimal ("10000"));
+		List<BankAccount> accounts = new ArrayList<>();
+		accounts.add(check);
+		accounts.add(save);
+		accounts.add(bank);
 	}
 
 	@Test
@@ -37,6 +47,11 @@ public class BankCustomerTest {
 	public void addAccountTest(){
 		customer.addAccount(bank);
 		assertEquals(bank, customer.getAccounts().get(0));
+	}
+	
+	@Test
+	public void isVIPTest() {
+		customer.isVIP();
 	}
 
 }
